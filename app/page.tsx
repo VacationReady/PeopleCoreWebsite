c248375d8d6a64db529f5ab122c72165dd84f0bc
Michael Dowdle <michael.dowdle@hotmail.com>
2025-10-09 14:40:14 +0100

    Social Proof removed

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import { ValueProps } from "./components/sections/value-props"
 import { Features } from "./components/sections/features"
 import { DashboardShowcase } from "./components/sections/dashboard-showcase"
 import { UseCases } from "./components/sections/use-cases"
-import { SocialProof } from "./components/sections/social-proof"
 import { Pricing } from "./components/sections/pricing"
 import { Waitlist } from "./components/sections/waitlist"
 import { Footer } from "./components/sections/footer"
@@ -20,7 +19,6 @@ export default function Home() {
       <Features />
       <DashboardShowcase />
       <UseCases />
-      <SocialProof />
       <Pricing />
       <Waitlist />
       <Footer />
