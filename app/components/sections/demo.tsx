8ac5181eaaea7e221ae7b0a17413d1cf6b31598b
Michael <michael.dowdle@hotmail.com>
2025-11-16 20:08:18 +1300

    demo upgrade

--- app/components/sections/demo.tsx
+++ app/components/sections/demo.tsx
@@ -21,7 +21,7 @@ function SkeletonLine({ width = "100%" }: { width?: string }) {
 
 function WorkspaceGrid() {
   return (
-    <div className="absolute inset-0 opacity-[0.03]" style={{
+    <div className="pointer-events-none absolute inset-0 opacity-[0.03]" style={{
       backgroundImage: `
         linear-gradient(to right, rgb(148, 163, 184) 1px, transparent 1px),
         linear-gradient(to bottom, rgb(148, 163, 184) 1px, transparent 1px)
