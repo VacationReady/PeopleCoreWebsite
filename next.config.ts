39a000454259591baabfa8118ab496c24d523e83
Michael <michael.dowdle@hotmail.com>
2025-12-08 10:16:30 +1300

    Upgrade to next.js16

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // Disable ESLint during builds for now
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     // Enable TypeScript checking during builds
     ignoreBuildErrors: false,
