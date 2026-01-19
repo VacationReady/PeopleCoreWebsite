a6c6b7c88d9244d757a978238d9ef026b55b67cb
Michael <michael.dowdle@hotmail.com>
2026-01-19 17:08:22 +1300

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,6 +74,24 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>{children}</Providers>
+        {/*Start of Tawk.to Script*/}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/696dad8f0f26dd197d6619e7/1jfa6rv2a';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
+        {/*End of Tawk.to Script*/}
       </body>
     </html>
   );
